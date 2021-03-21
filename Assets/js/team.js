58b47718ec866088c09170e598dfc2e76d635855
Mohammad Darvish <mdschoolid@gmail.com>
2021-03-21 14:45:24 +0100

    Add files via upload

---
+++ Assets/js/team.js
@@ -0,0 +1,407 @@
+$(document).ready(function(){
+  $(".member1").hover(function(){
+  	$(this).addClass("scaleClass");
+  	$(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+  	$(this).animate({borderWidth:"0px"},500);
+  	$(".memImg").delay(500).fadeOut();
+  	$(".reveal").fadeIn();
+  	
+  },function(){
+  	$(this).removeClass("scaleClass");
+  	$(this).animate({borderRadius:"50%"},500);
+  	$(this).animate({borderWidth:"5px"},500);
+  	$(".memImg").fadeIn();
+  });
+  $(".1").click(function(){
+  	$(".reveal").fadeOut();
+  	$(".bio1").fadeIn();
+	});
+  $(".back1").click(function(){
+  	$(".bio1").fadeOut(500);
+  	$(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member2").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg1").delay(500).fadeOut();
+    $(".reveal1").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg1").fadeIn();
+  });
+  $(".2").click(function(){
+    $(".reveal1").fadeOut();
+    $(".bio2").fadeIn();
+  });
+  $(".back2").click(function(){
+    $(".bio2").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member3").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg2").delay(500).fadeOut();
+    $(".reveal2").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg2").fadeIn();
+  });
+  $(".3").click(function(){
+    $(".reveal2").fadeOut();
+    $(".bio3").fadeIn();
+  });
+  $(".back3").click(function(){
+    $(".bio3").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member4").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg3").delay(500).fadeOut();
+    $(".reveal3").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg3").fadeIn();
+  });
+  $(".4").click(function(){
+    $(".reveal3").fadeOut();
+    $(".bio4").fadeIn();
+  });
+  $(".back4").click(function(){
+    $(".bio4").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member5").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg4").delay(500).fadeOut();
+    $(".reveal4").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg4").fadeIn();
+  });
+  $(".5").click(function(){
+    $(".reveal4").fadeOut();
+    $(".bio5").fadeIn();
+  });
+  $(".back5").click(function(){
+    $(".bio5").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member6").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg5").delay(500).fadeOut();
+    $(".reveal5").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg5").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member7").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg6").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg6").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member8").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg7").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg7").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member9").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg8").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg8").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member10").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg9").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg9").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member11").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg10").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg10").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member12").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg11").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg11").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member13").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg12").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg12").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member14").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg13").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg13").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member15").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg14").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg14").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member16").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg15").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg15").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $(".member17").hover(function(){
+    $(this).addClass("scaleClass");
+    $(this).css("borderRadius","50%").animate({borderRadius:"15px"},500);
+    $(this).animate({borderWidth:"0px"},500);
+    $(".memImg16").delay(500).fadeOut();
+    $(".reveal").fadeIn();
+    
+  },function(){
+    $(this).removeClass("scaleClass");
+    $(this).animate({borderRadius:"50%"},500);
+    $(this).animate({borderWidth:"5px"},500);
+    $(".memImg16").fadeIn();
+  });
+  $(".1").click(function(){
+    $(".reveal").fadeOut();
+    $(".bio").fadeIn();
+  });
+  $(".back").click(function(){
+    $(".bio").fadeOut(500);
+    $(".reveal").fadeIn();
+  });
+});
